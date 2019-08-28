0dd01e3aa259c7098a5b9f4993bf4ca6df4b362d
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-08-28 11:53:27 +0800

    update startpage link

--- home_web/static/startpage.js
+++ home_web/static/startpage.js
@@ -3,19 +3,19 @@ const entries = [
     {
         group: "Local server",
         items: [
-            { name: "filebrowser", url: "https://www.baidu.com", icon: "fas fa-folder-open" },
-            { name: "aria2ng", url: "https://www.baidu.com", icon: "fas fa-cloud-download-alt" },
-            { name: "syncthing", url: "https://www.baidu.com", icon: "fas fa-sync-alt" },
-            { name: "pihole", url: "https://www.baidu.com", icon: "fab fa-spotify" },
+            { name: "filebrowser", url: "http://matrixgo.dynv6.net:8001", icon: "fas fa-folder-open" },
+            { name: "aria2ng", url: "http://matrixgo.dynv6.net:6880", icon: "fas fa-cloud-download-alt" },
+            { name: "syncthing", url: "http://matrixgo.dynv6.net:8384", icon: "fas fa-sync-alt" },
+            { name: "pihole", url: "http://matrixgo.dynv6.net:8034/admin", icon: "fab fa-spotify" },
+            { name: "app5", url: "https://www.baidu.com", icon: "fab fa-confluence" },
+            { name: "app6", url: "https://www.baidu.com", icon: "fab fa-d-and-d" },
+            { name: "app9", url: "https://www.baidu.com", icon: "fab fa-amazon" },
         ]
     }, {
         group: "Web server",
         items: [
-            { name: "app5", url: "https://www.baidu.com", icon: "fab fa-confluence" },
-            { name: "app6", url: "https://www.baidu.com", icon: "fab fa-d-and-d" },
             { name: "GitHub", url: "https://www.github.com", icon: "fab fa-github" },
-            { name: "Blog", url: "https://www.vuepress.com", icon: "fas fa-blog" },
-            { name: "app9", url: "https://www.baidu.com", icon: "fab fa-amazon" },
+            { name: "Blog", url: "https://tengshan2008.github.io", icon: "fas fa-blog" },
         ]
     },
 ];
@@ -34,14 +34,14 @@ createIconList = () => {
     let iconList = $("#iconList");
     let iconListEntry = $("#iconListEntry");
 
-    for(const groupEntry of entries) {
-        for(const item of groupEntry.items) {
+    for (const groupEntry of entries) {
+        for (const item of groupEntry.items) {
             const ileClone = iconListEntry.clone();
             const idName = (groupEntry.group + item.name).replace(/[^A-Za-z0-9]/g, 'x');
-            ileClone.find(".iconLink").each(function() { (this).href = item.url });
+            ileClone.find(".iconLink").each(function () { (this).href = item.url });
             ileClone.find(".iconIcon").addClass(item.icon);
             ileClone.find(".iconLink")[1].innerText = item.name;
-            ileClone.click(function() { window.location.href = item.url; });
+            ileClone.click(function () { window.location.href = item.url; });
             ileClone.prop("id", "iconListEntry" + idName).appendTo(iconList);
         }
     }
@@ -54,16 +54,16 @@ createGroupList = () => {
     let groupList = $("#groupList");
     let groupListEntry = $("#groupListEntry");
 
-    for(const groupEntry of entries) {
+    for (const groupEntry of entries) {
         const gleClone = groupListEntry.clone();
         const idName = groupEntry.group.replace(/[^A-Za-z0-9]/g, 'x');
         gleClone.find(".groupTitle")[0].innerText = groupEntry.group;
 
         const groupListList = gleClone.find(".groupListList");
         const groupListListItem = groupListList.find(".groupListListItem");
-        for(const item of groupEntry.items) {
+        for (const item of groupEntry.items) {
             const newListItem = groupListListItem.clone().appendTo(groupListList);
-            newListItem.find(".groupLink").each(function() { (this).href = item.url });
+            newListItem.find(".groupLink").each(function () { (this).href = item.url });
             newListItem.find(".groupIcon").addClass(item.icon);
             newListItem.find(".groupText")[0].innerText = item.name;
         }
@@ -77,8 +77,8 @@ createGroupList = () => {
     groupListEntry.remove();
 }
 
-$(function() {
-    if(groupMode) {
+$(function () {
+    if (groupMode) {
         createGroupList();
         $("#iconList").remove();
     } else {
